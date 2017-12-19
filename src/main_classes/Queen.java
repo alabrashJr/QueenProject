45897d9eab5d54e0dc771fa5abe3afd2a35a1761
abode965 <abode.himsy@gmail.com>
2017-12-20 00:38:52 +0200

    add and implement the ColorUtils class ++ cleaning :)

--- src/main_classes/Queen.java
+++ src/main_classes/Queen.java
@@ -3,12 +3,15 @@ package main_classes;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.paint.Color;
 import utilities_classes.AbstractGraph;
+import utilities_classes.ColorUtils;
 import utilities_classes.UnweightedGraph;
 
 public class Queen {
@@ -16,19 +19,17 @@ public class Queen {
     public Queen() {
         main();
     }
-    
+    private static Map<String, List<Integer>> colorMap 
+            = new HashMap< String, List<Integer>>();
     private static UnweightedGraph g;
     private static final Color[] colors = {Color.CYAN, Color.GRAY, Color.MAGENTA, Color.LIGHTGRAY, Color.BLACK, Color.BLUE, Color.GREEN, Color.PURPLE, Color.YELLOW, Color.ORANGE,
         Color.RED, Color.PINK, Color.ALICEBLUE, Color.AQUA, Color.BROWN, Color.FUCHSIA, Color.MISTYROSE};
-    private static ArrayList<Color> Vcolors;
+    private static ArrayList<Color> Vcolors; //to color the vertices , it should be passed to DisplayQueen Class 
+
     public static void main() {
         String path = "C:\\Users\\EDAAT\\Desktop\\queen11_11.col";
-        g = (UnweightedGraph) readUnweightedGraphFromFile(path);
-       //  g.printEdges();
+        g =readUnweightedGraphFromFile(path);
         Vcolors = heuristic(g);
-      //  Queen q = new Queen();
-        //int[][]temp = q.getEdgeList();
-        System.out.println("    ");
     }
 
     public static UnweightedGraph readUnweightedGraphFromFile(String path) {
@@ -59,7 +60,7 @@ public class Queen {
     public static ArrayList<Color> heuristic(UnweightedGraph g) {
         boolean[][] colorVer = new boolean[g.getSize()][g.getSize()];
         int j = 0;
-        //int[] c = new int[g.getSize()];
+        ColorUtils cUtils = new ColorUtils();
         ArrayList<Color> verticesColor = new ArrayList<>();
         for (int i = 0; i < g.getSize(); i++) {
             for (int k = 0; k < g.getSize(); k++) {
@@ -75,23 +76,41 @@ public class Queen {
             for (Object v : g.getNeighbors(i)) {
                 colorVer[(int) v][j] = true;
             }
-            //c[i] = (Integer) j;
-            System.out.println(i + "   i >>>>>>>>>> j " + j);
-            verticesColor.add(i, colors[j]);// i get strengly an ArrayIndexOutOfBoundsException 
+            String colorName = cUtils.getColorNameFromHex(colors[j].hashCode()); // get the name of color using it hash number
+            System.out.println(i + ".vertex is colored with     " + colorName);
+            verticesColor.add(i, colors[j]);
+
+            // sparet the vertices by them color and make a hashtable 
+            List<Integer> temp = colorMap.get(colorName);
+            if (temp == null) {
+                temp = new ArrayList<>();
+            }
+            temp.add(i);
+            colorMap.put(colorName, temp);
 
         }
+        //print the hash table 
+        for (String name : colorMap.keySet()) {
+            String key = name.toString();
+            List<Integer> value = colorMap.get(name);
+            System.out.print(key+ "--->");
+            for (int i = 0; i < value.size(); i++) {
+                System.out.print(value.get(i)+"  ");
+            }
+            System.out.println("");
+        }System.out.println("we used "+colorMap.size()+" colors to color the graph ");
         return verticesColor;
     }
 
     public int getSize() {
         return g.getSize();
-    }
+    } //used in DisplayQueen to find the size of graph on Queen Class 
 
-    public  Color getColor(int i) {
+    public Color getColor(int i) { // used in DisplayQueen to find what the color of i.vertex 
         return Vcolors.get(i);
     }
 
-    public int[][] getEdgeList(){
+    public int[][] getEdgeList() {
         return g.getEdgeList();
-    }
+    } // inorder to init the graph on DisplayQueen 
 }
